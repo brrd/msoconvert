82d177ecb10902414997a2b55a96412c182f5219
Thomas Brouard <contact@brrd.fr>
2021-04-01 22:20:20 +0200

    Add test "Convert directory"

--- test.js
+++ test.js
@@ -22,18 +22,36 @@ const emptyDir = (dirpath) => {
 
 const fileExists = (s) => new Promise(r => fs.access(s, fs.F_OK, e => r(!e)));
 
+const allFilesExist = (f) => Promise.all(f.map(fileExists)).then(r => r.every(v => v === true));
+
 test.beforeEach(() => {
   const outDir = path.join(__dirname, "test/out");
   return emptyDir(outDir);
 });
 
-test.serial("Convert a single DOCX document", async t => {
+test.serial("Convert a single document", async t => {
   const options = {
-    input: "test/src/test.docx",
-    output: "test/out/test.html",
-    format: "wdFormatHTML"
+    input: "test\\src\\test.docx",
+    output: "test\\out\\test.doc",
+    format: "wdFormatDocument97"
   };
-  await convert.file(options);
+  await convert(options);
   const exists = await fileExists(options.output);
+  t.true(exists);
+});
+
+test.serial("Convert directory", async t => {
+  const options = {
+    input: "./test/src",
+    inputExt: "\"*.docx\"",
+    output: "./test/out",
+    outputExt: "doc",
+    format: "wdFormatDocument97"
+  };
+  await convert(options);
+  const exists = await allFilesExist([
+    "test\\out\\test.doc",
+    "test\\out\\test2.doc"
+  ]);
   t.true(exists);
 });
